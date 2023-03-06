7e5f414005d85bee1928f5dd16d0974ff17e6e0c
Yiren Lu <yirenlu13@gmail.com>
2023-03-06 18:21:59 -0500

    Clean up footer

--- data/site.ts
+++ data/site.ts
@@ -3,7 +3,7 @@ export const site = {
   description: 'Conversations with friends on tech and business, edited with AI.',
   // This should be a url to the static file you're using for your open graph image
   ogImage: 'https://fresh-blog.littlesticks.dev/og-image.png',
-  copyrightName: 'Fresh Blog',
+  copyrightName: 'Yiren Lu',
   // The url of the source code for the site, used in the footer.
   viewSourceUrl: 'https://github.com/littlesticks/fresh-blog'
 }
